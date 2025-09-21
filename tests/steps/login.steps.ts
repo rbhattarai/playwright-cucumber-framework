10434565affefe464b18b9a2bdefb75b7c936a5b
Rohan Bhattarai <rohan.bhattarai@gmail.com>
2025-09-21 18:09:20 -0400

    Updated steps

--- tests/steps/login.steps.ts
+++ tests/steps/login.steps.ts
@@ -4,10 +4,12 @@ import { CustomWorld } from '@support/world';
 
 Given('I open Google', async function(this: CustomWorld) {
   await this.page.goto('https://www.google.com');
+  await this.page.waitForLoadState('networkidle');
 });
 
 Then('The title should contain {string}', async function(this: CustomWorld, title: string) {
   const pageTitle = await this.page.title();
+  await this.page.waitForLoadState('networkidle');
   if (!pageTitle.includes(title)) {
     throw new Error(`Expected title to include "${title}" but got "${pageTitle}"`);
   }
